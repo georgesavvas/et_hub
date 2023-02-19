fcf89c52aedd995a950d01830b5245b0be9c2b41
George <cgeorgesavvas@gmail.com>
2023-02-19 21:56:12 +0000

    wip

--- src/views/widgets/Projects.jsx
+++ src/views/widgets/Projects.jsx
@@ -15,25 +15,20 @@ const files = [
 ];
 
 const Projects = props => {
-  console.log(props.size);
-  // const view_width = props.size.width - 50;
-  // const job_width = (props.size.height - 95) * (16/9);
-  // let amount = Math.floor(view_width / (job_width+40));
-  // amount = Math.max(amount, 1);
-  // const centre = (amount !== 1);
-  // console.log(view_width + " / " + job_width + " = " + amount + " " + centre);
-
   const slideHeight = props.size[1];
-  const slideWidth = slideHeight * 19/6;
-  const slidesAmount = slideWidth / props.size[0];
+  const slideWidth = slideHeight * 16/9;
+  const slidesAmount = Math.max(props.size[0] / slideWidth, 1);
   const slideStyle = {
     width: `${slideWidth}px`,
     height: `${slideHeight}px`
   };
+  const slidesToScroll = Math.round(slidesAmount / 2);
+  console.log(slidesAmount, slidesToScroll);
   return (
     <div className={styles.container}>
-      {/* {files.map(file => <video key={file} className={styles.video} src={`media/jobs/${file}.mp4`} />)} */}
-      <Carousel adaptiveHeight autoplay wrapAround slidesToShow={slidesAmount} autoplayInterval={5000} cellAlign="center">
+      <Carousel autoplay wrapAround enableKeyboardControls
+        slidesToScroll={slidesToScroll} dragThreshold={0.25}
+        slidesToShow={slidesAmount} autoplayInterval={5000} cellAlign="center">
         {files.map(file =>
           <div key={file} style={slideStyle}>
             <Typography style={{position: "absolute"}} align="center">{file}</Typography>
